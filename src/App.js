e4668cf565b6bc8d42a17c55167e4054d93cb9ae
Harini-Thandoju <harinithandoju@gmail.com>
2022-01-16 19:12:15 +0530

    book my show project frontend with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import SliderImages from './components/SliderImages';
+import CardSlider from './components/CardSlider';
+import Premiers from './components/Premiers';
+import Footer from './components/Footer/Footer';
+import AllMovies from './components/AllMovies';
+import SingleMovie from './components/SingleMovie';
+// import AllMoviesFetch from './components/AllMoviesFetch';
+// import SingleMovieFetch from './components/SingleMovieFetch';
+import Registration from './components/Registration';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      <Router>
+          <div>
+    <Header />
+    <Switch>
+    <Route exact path="/">{/*If the path is this it is applicable for included sliders slider images,card sliders*/ }
+          <SliderImages />
+            <CardSlider/>
+            <Premiers />
+            </Route>
+          {/*If the path is this (movies) it is applicable for included sliders which is premiers nd exact used for avoid confuction like if /movies is there frst it takes the slides of / not movies becoz it is present at the frst /*/ }
+          <Route exact path="/movies">
+            <AllMovies/>
+            {/* <AllMoviesFetch /> */}
+          </Route>
+           <Route exact path="/movies/:movid">
+             <SingleMovie />
+          {/* <SingleMovieFetch /> */}
+          </Route> 
+          <Route exact path="/register">
+          <Registration />
+          </Route> 
+        </Switch>
+    <Footer />
     </div>
+    </Router>
   );
 }
 
 export default App;
+// {/*header nd footer is outside the swith becoz header nd footer is common for all pages*/}
